a0eedfb0b69541763270c7c38059e083036adac8
Gorka Alvarez <gorka.alvarezr@alumni.mondragon.edu>
2024-05-29 12:07:45 +0200

    Fase 4 finalizada

--- Consulta.R
+++ Consulta.R
@@ -1,4 +1,4 @@
 df<- read.csv("DF_DC_VO.csv")
 
-df %>% group_by(Area) %>% 
+df %>% group_by(Area, Date.Analyzed) %>% 
   summarize_at(vars(Value, MDL, RDL), mean, na.rm=T)
--- Integración.R
+++ Integración.R
@@ -1,6 +1,6 @@
 getwd()
 setwd("C:/Users/Sara Uni/Desktop/BDATA1/DATA SCIENCE I/Reto4/Sara_Gorka_Cooperativo")
-water <- read.csv("water-quality-data.csv")
+water <- read.csv("water-quality-context.csv")
 df_na <- read.csv("DF_DC_na.csv")
 
 juntar <- merge(water, df_na)
